b73d848c4769d1c99b4b49f122bb03c1d622e012
KasiaKos81 <kasiakos81@gmail.com>
2022-05-07 15:39:05 +0200

    Tablica

--- src/main/java/pl/sda/arp4/collections/Main.java
+++ src/main/java/pl/sda/arp4/collections/Main.java
@@ -1,4 +1,25 @@
 package pl.sda.arp4.collections;
 
 public class Main {
-}
+    public static void main(String[] args) {
+        // deklaracja tablicy
+        int[] tablica = new int[5]; // deklaracja zer
+        int[] tablicaDeklaracja = new int[]{5, 10, 15, 20, 25};
+        tablica[0] = 1; // przypisanie do komórki 0 wartości 1
+        tablica[1] = 2;
+        tablica[2] = 20;
+        tablica[3] = -30;
+        tablica[4] = 75;
+
+       // for (int i = 0; i < tablica.length; i++) {
+         //   System.out.println(tablica);
+
+        for (int i : tablica) {
+            System.out.println(i);
+        }
+
+    }
+
+    }
+
+
